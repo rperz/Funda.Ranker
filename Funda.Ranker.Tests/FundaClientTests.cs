7299fa870b49537c37f73d6c905739b18537f500
Robin Perz | Media Buying Systems <robin@mediabuyingsystems.com>
2020-01-19 16:34:21 +0100

    Fixed the test

--- Funda.Ranker.Tests/FundaClientTests.cs
+++ Funda.Ranker.Tests/FundaClientTests.cs
@@ -16,8 +16,8 @@ namespace Funda.Ranker.Tests
         public async Task GetObjectsForSaleShouldReturnNumberOfItemsRequested()
         {
             var logger = new Mock<ILogger>();
-            var sut = new FundaClient(logger.Object, new FundaConfiguration() { MaxRetries = 10, SleepTimeAfterExceedingRequestLimit = 5000, BaseUrl = "http://partnerapi.funda.nl/feeds/Aanbod.svc/json/ac1b0b1572524640a0ecc54de453ea9f/?type={0}{1}&page={2}&pagesize={3}" });
-            var result = await sut.GetObjects(ListingType.Sale, 1, 25, 1, "tuin", "amsterdam").ConfigureAwait(false);
+            var sut = new FundaClient(logger.Object, new FundaConfiguration() { MaxRetries = 20, SleepTimeAfterExceedingRequestLimit = 5000, BaseUrl = "http://partnerapi.funda.nl/feeds/Aanbod.svc/json/ac1b0b1572524640a0ecc54de453ea9f/?type={0}{1}&page={2}&pagesize={3}" });
+            var result = await sut.GetObjects(ListingType.Sale, 1, 25, 1,  "amsterdam", "tuin").ConfigureAwait(false);
 
             Assert.AreEqual(25, result.Result.Count());
         }
@@ -26,7 +26,7 @@ namespace Funda.Ranker.Tests
         public async Task GetObjectsForSaleShouldFinishAllRequestsThrowExceptionWhenMoreThan100RequestsWithinAMinute()
         {
             var logger = new Mock<ILogger>();
-            var sut = new FundaClient(logger.Object, new FundaConfiguration() { MaxRetries = 10, SleepTimeAfterExceedingRequestLimit = 5000, BaseUrl = "http://partnerapi.funda.nl/feeds/Aanbod.svc/json/ac1b0b1572524640a0ecc54de453ea9f/?type={0}{1}&page={2}&pagesize={3}" });
+            var sut = new FundaClient(logger.Object, new FundaConfiguration() { MaxRetries = 20, SleepTimeAfterExceedingRequestLimit = 5000, BaseUrl = "http://partnerapi.funda.nl/feeds/Aanbod.svc/json/ac1b0b1572524640a0ecc54de453ea9f/?type={0}{1}&page={2}&pagesize={3}" });
             var tasks = new Task[150];
             for (int i = 0; i < 150; i++)
             {
